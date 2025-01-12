e244ddfd30abf30f84789397996c0a7c1680da60
manuelpravia <praviapuri@gmail.com>
2025-01-11 23:20:03 -0500

    feat: version tradicional http

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,8 +1,9 @@
-import { Injectable, Logger, NotFoundException, OnModuleInit } from '@nestjs/common';
+import { HttpStatus, Injectable, Logger, OnModuleInit } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { PrismaClient } from '@prisma/client';
 import { PaginationDto } from 'src/common';
+import { RpcException } from '@nestjs/microservices';
 
 
 @Injectable()
@@ -48,7 +49,10 @@ export class ProductsService extends PrismaClient implements OnModuleInit{
     });
 
     if(!product){
-      throw new NotFoundException(`Product with id ${id} not found`);
+      throw new RpcException({
+        message: `Product with id ${id} not found`,
+        status: HttpStatus.BAD_REQUEST
+    });
     }
     return product;
   }
@@ -76,6 +80,28 @@ export class ProductsService extends PrismaClient implements OnModuleInit{
       }
     })
 
+  }
+
+  async validateProducts(ids: number[]) {
+    //Quitamos los duplicados
+    ids = Array.from(new Set(ids));
+
+    const products = await this.product.findMany({
+      where: {
+        id: {
+          in: ids
+        }
+      }
+    });
+
+    if(products.length !== ids.length){
+      throw new RpcException({
+        message: `Some products were not found`,
+        status: HttpStatus.BAD_REQUEST
+      })
+    }
+    return products;
+    
   }
 
 
