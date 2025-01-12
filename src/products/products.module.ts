5a8b2bcc64586f4a62dedd6078084d69c72f05ea
manuelpravia <praviapuri@gmail.com>
2025-01-12 02:12:58 -0500

    feat configure nats

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ProductsService } from './products.service';
 import { ProductsController } from './products.controller';
+import { NatsModule } from 'src/transport/nats.module';
 
 @Module({
   controllers: [ProductsController],
   providers: [ProductsService],
+  imports: [
+    NatsModule
+  ]
 })
 export class ProductsModule {}
